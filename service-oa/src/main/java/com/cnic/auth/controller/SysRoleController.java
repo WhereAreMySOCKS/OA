2acef856457588e8990cbce21efea5b138ff195c
paul <957819893@qq.com>
2023-06-03 21:54:09 +0800

    add knife4j

--- service-oa/src/main/java/com/cnic/auth/controller/SysRoleController.java
+++ service-oa/src/main/java/com/cnic/auth/controller/SysRoleController.java
@@ -3,18 +3,21 @@ package com.cnic.auth.controller;
 import com.cnic.auth.service.SysRoleService;
 import com.cnic.common.result.Result;
 import com.cnic.model.system.SysRole;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+@Api("角色管理接口")
 @RestController //注册controller，交给Spring管理，同时以json文件格式返回方法结果
 @RequestMapping("/admin/system/sysRole")
 public class SysRoleController {
     @Autowired
     private SysRoleService sysRoleService;
+    @ApiOperation("查询所有角色")
     @GetMapping("findAll") // http://localhost:8800/admin/system/sysRole/findAll
     public Result findAll(){
         List<SysRole> list = sysRoleService.list();
