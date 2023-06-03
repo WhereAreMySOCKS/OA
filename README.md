8855da87cc0ebb6dca4b94eec8f2f91ae0b61faa
paul <957819893@qq.com>
2023-06-03 18:36:56 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# 基于SpringBoot的办公办理系统
+
+## 特性
+
+* 使用Mybatis-plus
+
+## 项目结构
+
+    .
+    ├── .idea                   # idea自动生成的配置文件
+    ├── common                  # 存放util类
+    │   ├── common-util
+    │   │   ├── src
+    │   │   ├── target
+    │   │   └── pom.xml
+    │   ├── service-util
+    │   │   ├── src
+    │   │   ├── target
+    │   │   └── pom.xml
+    │   └── pom.xml
+    ├── model   
+    │   ├── src
+    │   ├── target
+    │   └── pom.xml
+    ├── service-oa  
+    │   ├── src
+    │   ├── target
+    │   └── pom.xml
+    ├── pom.xml  
+    └── README.md  
+
+
+
+
---
+++ service-oa/src/main/java/com/cnic/auth/controller/SysRoleController.java
@@ -0,0 +1,24 @@
+package com.cnic.auth.controller;
+
+import com.cnic.auth.service.SysRoleService;
+import com.cnic.model.system.SysRole;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController //注册controller，交给Spring管理，同时以json文件格式返回方法结果
+@RequestMapping("/admin/system/sysRole")
+public class SysRoleController {
+    @Autowired
+    private SysRoleService sysRoleService;
+    @GetMapping("findAll") // http://localhost:8800/admin/system/sysRole/findAll
+    public List<SysRole> findAll(){
+        List<SysRole> list = sysRoleService.list();
+        // @RestController注解会将list转化成json格式返回
+        return list;
+
+    }
+}
